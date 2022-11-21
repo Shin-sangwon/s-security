b16ea4ca8bc847875c098f17b4e0f31f444d9579
Shin-sangwon <dnjsml10@gmail.com>
2022-11-21 21:45:37 +0900

    feat : add User repo

---
+++ src/main/java/com/security/data/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.security.data.repository;
+
+import com.security.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
